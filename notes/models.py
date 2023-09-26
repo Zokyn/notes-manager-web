e516008667685d4b62d5884ced5c10565a015c77
Gustavo Casaes <gustavo.casaes@mail.uft.edu.br>
2023-09-26 17:26:38 -0300

    adicionado detalhes

--- notes/models.py
+++ notes/models.py
@@ -4,17 +4,17 @@ from django.db import models
 from django.utils import timezone
 
 class Note(models.Model):
-    title_note = models.CharField(max_length=200)
+    note_title = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
     def __str__(self):
-        return self.title_note
+        return self.note_title
 
 class Body(models.Model):
     note = models.ForeignKey(Note, on_delete=models.CASCADE)
     body_text = models.TextField(max_length=456)
-    votes = models.IntegerField(default=0)
+    upvotes = models.IntegerField(default=0)
     def __str__(self):
         return self.body_text
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.template import loader 
+from django.template import loader
+from django.urls import reverse
 from .models import Note
 
 # Create your views here.
@@ -19,4 +20,20 @@ def results(request, note_id):
     return HttpResponse("You're looking at a body %s" % note_id)
 
 def vote(request, note_id):
-    return HttpResponse("You're liking a body %s note" % note_id)
+    note = get_object_or_404(Note, pk=note_id)
+    try: 
+        selected_body = note.body_set.get(pk=request.POST["body"])
+    except (KeyError, Note.DoesNotExist):
+        return render(
+            request,
+            "notes/detail.html",
+            {
+                "note": note,
+                "error_message": "You didn't select a choice"
+            },
+        ) 
+    else: 
+        selected_body.upvotes += 1
+        selected_body.save()
+
+    return HttpResponseRedirect(reverse("notes:body", args=((note.id,))))
