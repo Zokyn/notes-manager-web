238449b133b19cf6a54b3e951386711077ea5314
Gustavo Casaes <gustavo.casaes@mail.uft.edu.br>
2023-10-02 16:38:07 -0300

    adicionado pagina de novo body; funcionando.

--- notes/views.py
+++ notes/views.py
@@ -3,15 +3,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.urls import reverse
 from django.contrib import messages
+from django.views.generic.list import ListView
+from .forms import BodyForm
 from .models import Note, Body
 
 # Create your views here.
-def index(request):
-    latest_note_list = Note.objects.order_by("-pub_date")[:10]
-    context = {
-        "latest_note_list": latest_note_list
-    }
-    return render(request, "notes/index.html", context)
+
+class NoteListView(ListView):
+    model = Note
+    context_object_name = "latest_note_list"
+    template_name = "notes/index.html"
 
 def note(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
@@ -29,6 +30,20 @@ def note(request, note_id):
         return render(request, "notes/note.html", context)
     return render(request, "notes/note.html", context)
 
+def new(request, note_id):
+    note = Note.objects.get(pk=note_id)
+    
+    if request.method == 'POST':
+        form = BodyForm(request.POST)
+        if form.is_valid():
+            body = form.save(commit=False)
+            body.note = note
+            body.save()
+        return redirect('notes:index')
+    else:
+        form = BodyForm()
+    return render(request, "notes/new.html", {'note': note, 'form': form})
+    
 def edit(request, note_id, body_id):
     note = get_object_or_404(Note, pk=note_id)
     try: 
