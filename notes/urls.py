661054ace67587121e0616049a282c28a51112ea
Gustavo Casaes <gustavo.casaes@mail.uft.edu.br>
2023-10-02 17:15:18 -0300

    modificado o formulario de 'edit.html'; resolver o sumico do conteudo do texto

--- notes/urls.py
+++ notes/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("", views.NoteListView.as_view(), name="index"),
     path("<int:note_id>/", views.note, name="note"),
     path("<int:note_id>/new/", views.new, name="new"),
-    path("<int:note_id>/post/", views.post, name="post"),
     path("<int:note_id>/edit/<int:body_id>", views.edit, name="edit"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -45,33 +45,14 @@ def new(request, note_id):
     return render(request, "notes/new.html", {'note': note, 'form': form})
     
 def edit(request, note_id, body_id):
-    note = get_object_or_404(Note, pk=note_id)
-    try: 
-        body = get_object_or_404(Body, pk=body_id)
-        context = {
-            "note": note,
-            "body": body
-        }
-        return render(request, 'notes/edit.html', context)
-    except (KeyError, Body.DoesNotExist):
-        error = "Can't reach selected body. This note doesn't have this body linked"
-        messages.error(request, error)
-    return redirect('notes:index')
-
-def post(request : HttpRequest, note_id):
-    note = get_object_or_404(Note, pk=note_id)
-    if request.method == "POST":
-        body = get_object_or_404(Body, pk=request.POST["body_id"])
-        try: 
-            body.body_text = request.POST.get("text", '')
+    note = Note.objects.get(pk=note_id)
+    body = Body.objects.get(pk=body_id)
+    
+    if request.method == 'POST':
+        form = BodyForm(request.POST, instance=body)
+        if form.is_valid():
             body.save()
-        except (KeyError, Note.DoesNotExist):
-            context = {
-                "note": note,
-                "error_message": "Essa nota não existe"
-            }
-            return render(request, "notes:note", context)
+            return redirect('notes:index')
     else:
-        error = "Request doens't have method post"
-        messages.error(request, error)
-    return redirect("notes:index")
+        form = BodyForm(instance=body)
+    return render(request, "notes/edit.html", {'note': note, 'body': body, 'form': form})
