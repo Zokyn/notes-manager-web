102f638848ceb6951e6e8475926ae924bf51711e
Gustavo Casaes <gustavo.casaes@mail.uft.edu.br>
2023-10-11 16:19:19 -0300

    adicionado bootstrap e statics em css

--- notes/urls.py
+++ notes/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views 
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path("<int:note_id>/", views.note, name="note"),
     path("<int:note_id>/new/", views.new, name="new"),
     path("<int:note_id>/edit/<int:body_id>", views.edit, name="edit"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
